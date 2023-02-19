e8429a5728671869f94d1d9efa86eac2e625dd05
Nisarg <nisargs2001@gmail.com>
2023-02-20 00:10:13 +0530

    frontend setup

---
+++ frontend/src/components/HeroSection.js
@@ -0,0 +1,30 @@
+import hero from '../assets/hero.PNG'
+const HeroSection = () => {
+  return (
+    <div className="flex flex-row justify-between bg-black text-white px-10 py-20 pb-40 hero items-center">
+      <div>
+        <h1 className="text-5xl font-extrabold mb-10">
+          Reach your - <br />
+          audience & convert your leads
+        </h1>
+        <p className="mb-6">
+          Get your customers with SEO, rank your business with <br /> email
+          marketing, build sales leads
+        </p>
+        <div>
+          <button className="get-btn text-black font-bold px-3 py-1 rounded-md mr-5">
+            Get Started
+          </button>
+          <button className="bg-black text-white font-bold px-3 py-1 rounded-md border-solid border-white border">
+            Watch Demo
+          </button>
+        </div>
+      </div>
+      <div>
+        <img  className="w-[500px]" src={hero} alt=""/>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,22 @@
+
+const Navbar = () => {
+  return (
+    <div className="flex flex-row justify-between items-center bg-black text-white px-10 py-5">
+        <div className="font-bold text-xl">
+            MARKALL
+        </div>
+        <div>
+            <a href="/#" className="mr-5">Services</a>
+            <a href="/#" className="mr-5">Pricing</a>
+            <a href="/#">About Us</a>
+        </div>
+        <div>
+            <button className="get-btn text-black font-bold px-3 py-1 rounded-md">
+                Get Started
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
